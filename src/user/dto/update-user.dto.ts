75a6f4307f7043c2d574afbf5815b2e4e9708c44
Jino <pen40@naver.com>
2022-08-26 15:16:40 +0900

    2022-08-26 user - update dto 변경

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -4,9 +4,6 @@ export class UpdateUserDto {
   @ApiProperty({description : `사용자 명`})
   readonly user_name : string;
 
-  @ApiProperty({description : `사용자 닉네임`})
-  readonly user_nickname : string;
-
   @ApiProperty({description : `사용자 전화번호`})
   readonly user_phone_number : string;
 }
